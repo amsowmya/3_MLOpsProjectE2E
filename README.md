364eebfee485cfceef6ca0f6a5e92ad72322f991
amsowmya <soumya.pdit@gmail.com>
2024-04-11 16:17:49 +0530

    added airflow code

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 bash init_setup.sh
 
+# Airflow terms
+ti : task instance / task information 
+xcom_push : cross communication push
+xcom_pull : cross communication pull
+
+# DVC 
+dvc init
+
+# add the files to local remote storage
+dvc remote add -d remote_storage my_remote_storage
+
+# pipeline reproducability
+dvc repro
+
+# Deployment
+docker build -t youtubelive.azure.io/youtubecomm:latest .
+docker login youtubelive.azure.io
+username
+password
+
+docker push youtubelive.azure.io/youtubecomm:latest
--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -9,16 +9,52 @@ from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
 from src.components.model_evaluation import ModelEvaluation
 from src.components.model_trainer import ModelTrainer
+import os 
+import sys 
 
-obj = DataIngestion()
+# obj = DataIngestion()
 
-train_data_path, test_data_path = obj.initiate_data_ingestion()
+# train_data_path, test_data_path = obj.initiate_data_ingestion()
 
-data_transformation = DataTransformation()
-train_arr, test_arr = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+# data_transformation = DataTransformation()
+# train_arr, test_arr = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
 
-model_trainer_obj = ModelTrainer()
-model_trainer_obj.initiatemodel_trainer(train_arr, test_arr)
+# model_trainer_obj = ModelTrainer()
+# model_trainer_obj.initiatemodel_trainer(train_arr, test_arr)
 
-model_eval_obj = ModelEvaluation()
-model_eval_obj.initiate_model_evaluation(train_arr, test_arr)
+# model_eval_obj = ModelEvaluation()
+# model_eval_obj.initiate_model_evaluation(train_arr, test_arr)
+
+class TrainingPipeline:
+    def start_data_ingestion(self):
+        try:
+            data_ingestion = DataIngestion()
+            train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+            return train_data_path, test_data_path
+        except Exception as e:
+            raise customexception(e, sys)
+        
+    def start_data_transformation(self, train_data_path, test_data_path):
+        try:
+            data_transformation = DataTransformation()
+            train_arr, test_arr = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+            return train_arr, test_arr
+        except Exception as e:
+            raise customexception(e, sys)
+        
+    def start_model_training(self, train_arr, test_arr):
+        try:
+            model_triner = ModelTrainer()
+            model_triner.initiatemodel_trainer(train_arr, test_arr)
+        except Exception as e:
+            raise customexception(e, sys)
+        
+    def start_training(self):
+        try:
+            train_data_path, test_data_path = self.start_data_ingestion()
+            train_arr, test_arr = self.start_data_transformation(train_data_path, test_data_path)
+            self.start_model_training(train_arr, test_arr)
+        except Exception as e:
+            raise customexception(e, sys)
+        
+        
